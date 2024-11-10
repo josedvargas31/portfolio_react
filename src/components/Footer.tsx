ec00fb711d23a0cf19c4559091def7995bf4f838
Alejo6648 <dajozavargas@gmail.com>
2024-11-09 21:17:18 -0500

    Agregar footer a la vista de inicio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,28 @@
-
 function Footer() {
-  return (
-    <>
-      <h1>holis! soy el foooter!</h1>
-    </>
-  )
+	return (
+		<>
+			<div className="flex flex-row h-20">
+				<h1 className="pr-6 font-sans text-white/75 underline underline-offset-4 decoration-sky-200 hover:decoration-slate-500 cursor-pointer">
+					<a
+						href="https://www.linkedin.com/in/dario-jose-zamora-vargas-32b9aa318"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						LinkedIn
+					</a>
+				</h1>
+				<h1 className="font-sans text-white/75 underline underline-offset-4 decoration-sky-200 hover:decoration-slate-500 cursor-pointer">
+					<a
+						href="https://github.com/josedvargas31"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						GitHub
+					</a>
+				</h1>
+			</div>
+		</>
+	);
 }
 
-export default Footer
+export default Footer;
